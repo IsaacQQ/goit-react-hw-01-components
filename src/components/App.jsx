ec8d0c513c819597d5be7816031fe95e84b4ace0
Игорь <bujhtyz228@gmail.com>
2022-12-21 00:12:11 +0200

    smh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Profile } from "./Profile/Profile";
+import user from '../data/user.json'
+
 export const App = () => {
+  console.log(user)
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  <div>
+<Profile profile={user[0]}/>
+  </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,6 @@
+export const Profile = ( {profile} )=>{
+    console.log('профиль',profile)
+return <div>
+    <h2>{profile.username}</h2>
+</div>
+};
